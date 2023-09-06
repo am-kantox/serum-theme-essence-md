c3bff9b983cdcc9fa8865573f868344514e72768
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2023-09-06 13:49:19 +0200

    Initial insert of fork changes

--- mix.exs
+++ mix.exs
@@ -3,9 +3,9 @@ defmodule Serum.Themes.Essence.MixProject do
 
   def project do
     [
-      app: :serum_theme_essence,
-      version: "1.1.0",
-      elixir: "~> 1.7",
+      app: :serum_theme_essence_md,
+      version: "1.2.0",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package()
@@ -20,18 +20,19 @@ defmodule Serum.Themes.Essence.MixProject do
 
   defp deps do
     [
-      {:serum, ">= 1.1.0 or < 2.0.0"}
+      {:serum_md, ">= 1.1.0 or < 2.0.0"}
     ]
   end
 
   defp package do
     [
-      name: "serum_theme_essence",
-      description: "Essence is a Serum theme which presents a minimal and clean look.",
-      maintainers: ["Eunbin Jeong"],
+      name: "serum_theme_essence_md",
+      description:
+        "EssenceMD is a SerumMD theme which presents a minimal and clean look (ported from Essence).",
+      maintainers: ["Aleksei Matiushkin"],
       licenses: ["MIT"],
       links: %{
-        "GitHub" => "https://github.com/Dalgona/serum-theme-essence"
+        "GitHub" => "https://github.com/am-kantox/serum-theme-essence-md"
       }
     ]
   end
